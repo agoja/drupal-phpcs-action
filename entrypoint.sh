cede8f860129ba0a023dd710d04eee934b516c9a
AlexG <alexei.goja@unity3d.com>
2020-06-22 12:21:35 +0300

    WTM-7554: Drupal PHPCS

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,11 @@ echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
 echo "Review Drupal Standards"
 git log -1
-changed_files=$(git diff HEAD^ HEAD --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html" | grep "\/modules/unity\/" | grep "\\.install\|\\.module\|\\.inc\|\\.php\|\\.yml");
-#changed_files=$(git diff --cached --name-only --diff-filter=ACMR HEAD | grep "\/modules/unity\/" | grep "\\.install\|\\.module\|\\.inc\|\\.php\|\\.yml");
+changed_files=$(git diff HEAD^ HEAD --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html");
 echo -e "These are committed files for phpcs review:\n$changed_files";
 files_formatted="$(echo $changed_files | sed 's/ / .\//g')";
-
-if [[ ! -n "$files_formatted" ]]
-
-    then
-        phpcs --config-show
-        phpcs --report=checkstyle --ignore=.md,*.png,*.svg,*/dist/*,*.min.js,*.min.css,*/libraries/*,settings*,*/theme/* --standard=Drupal,DrupalPractice,DrupalSecure --encoding=utf-8 -p --extensions=module/php,php,install/php,js "./"$files_formatted
-    fi
+phpcs --config-show
+phpcs --report=checkstyle --ignore=.md,*.png,*.svg,*/dist/*,*.min.js,*.min.css,*/libraries/*,settings*,*/theme/* --standard=Drupal,DrupalPractice,DrupalSecure --encoding=utf-8 -p --extensions=module/php,php,install/php,js "./"$files_formatted
 
 status=$?
 
