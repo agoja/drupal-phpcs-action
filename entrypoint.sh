d8f99388279f0e6b0f49dcc6ff3f7dc353efb785
AlexG <alexei.goja@unity3d.com>
2020-06-22 12:30:57 +0300

    WTM-7554: Drupal PHPCS

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,19 @@ echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
 echo "Review Drupal Standards"
 git log -1
-changed_files=$(git diff HEAD^ HEAD --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html" | grep "\/modules/unity\/" | grep "\\.install\|\\.module\|\\.inc\|\\.php\|\\.yml");
-echo -e "These are committed files for phpcs review:\n$changed_files";
-files_formatted="$(echo $changed_files | sed 's/ / .\//g')";
-phpcs --config-show
-phpcs --report=checkstyle --ignore=.md,*.png,*.svg,*/dist/*,*.min.js,*.min.css,*/libraries/*,settings*,*/theme/* --standard=Drupal,DrupalPractice,DrupalSecure --encoding=utf-8 -p --extensions=module/php,php,install/php,js "./"$files_formatted
+changed_files=$(git diff HEAD^ HEAD --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html" | grep "\\.install\|\\.module\|\\.inc\|\\.php\|\\.yml");
 
-status=$?
-
-echo "::remove-matcher owner=phpcs::"
-
-exit $status
+if [[ -z "${changed_files}" ]];
+    then
+        status=$?
+        echo "::remove-matcher owner=phpcs::"
+        exit $status
+    else
+        echo -e "These are committed files for phpcs review:\n$changed_files";
+        files_formatted="$(echo ${changed_files} | sed 's/ / .\//g')";
+        phpcs --config-show
+        phpcs --report=checkstyle --ignore=.md,*.png,*.svg,*/dist/*,*.min.js,*.min.css,*/libraries/*,settings*,*/theme/* --standard=Drupal,DrupalPractice,DrupalSecure --encoding=utf-8 -p --extensions=module/php,php,install/php,js "./"${files_formatted}
+        status=$?
+        echo "::remove-matcher owner=phpcs::"
+        exit $status
+fi
