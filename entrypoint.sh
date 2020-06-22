768372f666daf217d2fccef6cc66a14fadae5266
AlexG <alexei.goja@unity3d.com>
2020-06-22 12:18:11 +0300

    WTM-7554: Drupal PHPCS

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,13 @@ changed_files=$(git diff HEAD^ HEAD --name-only --diff-filter=d --pretty="format
 #changed_files=$(git diff --cached --name-only --diff-filter=ACMR HEAD | grep "\/modules/unity\/" | grep "\\.install\|\\.module\|\\.inc\|\\.php\|\\.yml");
 echo -e "These are committed files for phpcs review:\n$changed_files";
 files_formatted="$(echo $changed_files | sed 's/ / .\//g')";
-phpcs --config-show
-phpcs --report=checkstyle --ignore=.md,*.png,*.svg,*/dist/*,*.min.js,*.min.css,*/libraries/*,settings*,*/theme/* --standard=Drupal,DrupalPractice,DrupalSecure --encoding=utf-8 -p --extensions=module/php,php,install/php,js "./"$files_formatted
+
+if [[ ! -n "$files_formatted" ]]
+
+    then
+        phpcs --config-show
+        phpcs --report=checkstyle --ignore=.md,*.png,*.svg,*/dist/*,*.min.js,*.min.css,*/libraries/*,settings*,*/theme/* --standard=Drupal,DrupalPractice,DrupalSecure --encoding=utf-8 -p --extensions=module/php,php,install/php,js "./"$files_formatted
+    fi
 
 status=$?
 
