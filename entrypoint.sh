84612af3fd10c361fc693ced3fcee3791705bdf2
AlexG <alexei.goja@unity3d.com>
2020-06-22 12:24:14 +0300

    WTM-7554: Drupal PHPCS

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
 echo "Review Drupal Standards"
 git log -1
-changed_files=$(git diff HEAD^ HEAD --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html");
+changed_files=$(git diff HEAD^ HEAD --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html" | grep "\/modules/unity\/" | grep "\\.install\|\\.module\|\\.inc\|\\.php\|\\.yml");
 echo -e "These are committed files for phpcs review:\n$changed_files";
 files_formatted="$(echo $changed_files | sed 's/ / .\//g')";
 phpcs --config-show
